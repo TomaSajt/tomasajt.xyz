8e8c85cdc8ed2d83386734ff8cc8879a7f4fc805
Toma <62384384+TomaSajt@users.noreply.github.com>
2021-10-10 17:54:36 +0200

    .

--- src/routes/__error.svelte
+++ src/routes/__error.svelte
@@ -14,8 +14,9 @@
 	export let error: any;
 </script>
 
-<div class="absolute inset-0 m-auto w-[fit-content] max-w-screen-md h-[fit-content] bg-gray-300 p-8 rounded-lg">
+<div
+	class="absolute inset-0 m-auto w-[fit-content] max-w-screen-md h-[fit-content] bg-gray-300 p-8 rounded-lg"
+>
 	<h1 class="text-center text-5xl font-bold">{status}</h1>
-	<h2 class="text-center text-2xl break-all">{error.message}</h2>
 	<h2>{error.stack}</h2>
 </div>
