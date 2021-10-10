8878b8af543b3f7670fa6a430b8d75a16568ece6
Toma <62384384+TomaSajt@users.noreply.github.com>
2021-10-10 17:53:07 +0200

    yoo

---
+++ src/routes/__error.svelte
@@ -0,0 +1,21 @@
+<script context="module" lang="ts">
+	export function load({ error, status }) {
+		return {
+			props: {
+				error: error,
+				status: status
+			}
+		};
+	}
+</script>
+
+<script lang="ts">
+	export let status: number;
+	export let error: any;
+</script>
+
+<div class="absolute inset-0 m-auto w-[fit-content] max-w-screen-md h-[fit-content] bg-gray-300 p-8 rounded-lg">
+	<h1 class="text-center text-5xl font-bold">{status}</h1>
+	<h2 class="text-center text-2xl break-all">{error.message}</h2>
+	<h2>{error.stack}</h2>
+</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,21 +1,9 @@
 <script lang="ts">
-	import FloatingLine from '$lib/components/FloatingLine.svelte';
-	const icons = [
-		{
-			src: '/assets/js.svg',
-			alt: 'JavaScript',
-			href: 'https://www.javascript.com/'
-		},
-		{
-			src: '/assets/karesz.png',
-			alt: 'Kareszhub',
-			href: 'https://kareszhub.tomasajt.xyz/'
-		},
-		{
-			src: '/assets/svelte.svg',
-			alt: 'SvelteKit',
-			href: 'https://kit.svelte.dev/'
-		}
+	import HomeBanner from '$lib/components/HomeBanner.svelte';
+	const links: { text: string; href: string }[] = [
+		{ href: '/hanoi', text: 'Towers of Hanoi' },
+		{ href: '/screen-recursion', text: 'Screen Recursion' },
+		{ href: '/file-editor', text: 'File Editor' }
 	];
 </script>
 
@@ -23,16 +11,14 @@
 	<title>Index | tomasajt.xyz</title>
 </svelte:head>
 
-<div class="bg-gray-800 h-96 text-white relative overflow-y-clip">
-	<div class="px-10 py-2">
-		<div class="font-bold text-2xl">Welcome to tomasajt.xyz</div>
-		<div class="font-medium text-2xl tracking-wider">by TomaSajt</div>
-	</div>
-
-	<div class="absolute w-full bottom-0 p-4">
-		<FloatingLine {icons} />
-	</div>
-</div>
+<HomeBanner />
+<ul class="m-8">
+	{#each links as { text, href }}
+		<li class="mb-4">
+			<a class="block w-[fit-content] px-4 py-2 bg-blue-500 text-white font-bold" {href}>{text}</a>
+		</li>
+	{/each}
+</ul>
 
 <style lang="postcss">
 </style>
