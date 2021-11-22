9edd22b3dd4c277c532e128bd2002d7064c29332
TomaSajt <62384384+TomaSajt@users.noreply.github.com>
2021-11-22 13:24:49 +0100

    pog

--- src/routes/rickspeed.svelte
+++ src/routes/rickspeed.svelte
@@ -7,15 +7,13 @@ import Button from "$lib/components/Button.svelte";
 <svelte:head>
     <title>Rick Speed</title>
 </svelte:head>
-    <video controls bind:playbackRate={hundrethSpeed} bind:this={video} loop>
+    <video class="max-w-screen-lg mx-auto" controls bind:playbackRate={hundrethSpeed} bind:this={video} loop>
         <source src="https://marci.hvj.hu/videos/rickroll.mp4">
         <track kind="captions">
     </video>
-    <div class="flex w-[min-content] mx-auto items-center flex-wrap">
-        <div>
-            <label for="speed" class="mr-4">{speed}%</label>
-            <input id="speed" type="range" bind:value={speed} min=10 max=400>
-        </div>
+    <div class="flex flex-col mx-auto max-w-screen-lg">
+        <label for="speed">{speed}%</label>
+        <input id="speed" type="range" bind:value={speed} min=10 max=400>
         <div>
             <Button onClick={()=>{
                 video.preservesPitch = !video.preservesPitch;
